ef8592614c805dbdbdb501b222b3d0999818a020
Heiko Hotz <heikohotz+git@gmail.com>
2024-12-29 13:29:22 +0000

    minor edits

--- part_3_vertex_api/chapter_12/shared/weather-api.js
+++ part_3_vertex_api/chapter_12/shared/weather-api.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-const OPENWEATHER_API_KEY = '04cc0e54d67a4ac16b87de310f1801a3';
+const OPENWEATHER_API_KEY = '<YOUR_OPENWEATHER_API_KEY>';
 
 export async function getWeather(city) {
   try {
